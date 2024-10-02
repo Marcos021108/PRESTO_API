5078ec657908d24129145e5abf10ee97214037bd
Marcos021108 <marcos.basso01@etec.sp.gov.br>
2024-10-02 08:36:56 -0300

    era pra ficar assim?

--- presto.py
+++ presto.py
@@ -1,14 +1,6 @@
 import  mysql.connector
 from fastapi import FastAPI
 
-conexao = mysql.connector.connect(
-    host='localhost',
-    user='root',
-    password="",
-    database="presto_bd"
-)
-cursor = conexao.cursor(dictionary=True)
-
 Presto = FastAPI()
 
 @Presto.get("/")
@@ -17,22 +9,43 @@ def home():
 
 @Presto.get("/usuarios")
 def usuarios():
+    conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password="",
+    database="presto_bd"
+    )
+    cursor = conexao.cursor(dictionary=True)
     comando = f'SELECT * FROM cliente'
     cursor.close()
     conexao.close()
 
 @Presto.get("/avaliacoes")
 def avaliacoes():
+    conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password="",
+    database="presto_bd"
+    )
+    cursor = conexao.cursor(dictionary=True)
     comando = f'SELECT * FROM avaliacao'
     cursor.close()
     conexao.close()
 
 @Presto.get("/pizzas")
 def pizza():
+    conexao = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    password="",
+    database="presto_bd"
+    )
+    cursor = conexao.cursor(dictionary=True)
     comando = f'SELECT * FROM pizza'
     cursor.close()
     conexao.close()
-
+    cursor.fetchall
 @Presto.get("/mentores")
 def mentores():
     return "obrigado gente"
